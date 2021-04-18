c8bd5db2771ca41405a9f35a9610191c0eae9b52
Rachel McCoy <rmccoy@colum.edu>
2021-04-18 11:04:21 -0500

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+const slider = document.querySelector('.slider');
+
+const leftArrow = document.querySelector('.left');
+
+const rightArrow = document.querySelector('.right');
+
+var sectionIndex = 0;
+
+leftArrow.addEventListener('click', function() {
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+    slider.style.transform = 'translate(' + (sectionIndex) * -33.33 + '%)';
+});
+
+rightArrow.addEventListener('click', function() {
+    sectionIndex = (sectionIndex < 2) ? sectionIndex + 1 : 2;
+    slider.style.transform = 'translate(' + (sectionIndex) * -33.33 + '%)';
+});
